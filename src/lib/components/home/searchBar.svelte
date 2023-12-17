b2cce9f1a200f22bdaa3c953b13ae8b8995262ac
awad238 <awad238>
2023-12-17 22:42:22 +0100

    home page ready

---
+++ src/lib/components/home/searchBar.svelte
@@ -0,0 +1,14 @@
+<section class="md:flex items-center justify-center mb-8">
+	<div class="flex border-2 rounded md:w-2/3 lg:w-1/2">
+		<input
+			class="border-0 w-full pl-2 focus:outline-none focus:ring-0 focus:shadow hover:placeholder:text-indigo-400 text-indigo-500 caret-indigo-600"
+			type="search"
+			placeholder="Search for notes..."
+		/>
+		<div>
+			<button class="border-l px-3 py-1 hover:bg-indigo-100 active:bg-indigo-200" type="submit"
+				>Search</button
+			>
+		</div>
+	</div>
+</section>
