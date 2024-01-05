76f9ccb1884750d06a1684b1bc49e61e37817083
awad238 <awad238>
2024-01-05 12:51:38 +0100

    import note complete

--- src/routes/edit/[noteId]/+page.svelte
+++ src/routes/edit/[noteId]/+page.svelte
@@ -9,6 +9,13 @@
 	export let data: PageData;
 	let source = data.note?.content;
 	let showCheatSheetModal = true;
+	let showImportModal = true;
+	let importNote: FileList;
+
+	// ! remove me
+	$: if (importNote) {
+		console.log(importNote);
+	}
 
 	// auto-save note every minute
 	onMount(() => {
@@ -77,6 +84,20 @@
 			success: 'Exported!',
 			error: 'An error occurred'
 		});
+	};
+
+	const handleImport = async (event: SubmitEvent) => {
+		try {
+			event.preventDefault();
+			// @ts-ignore
+			const files = event.currentTarget.importFile.files;
+			const file = files[0];
+			source = await file.text();
+			toast.success(`Successfully imported ${await file.name}!`);
+		} catch (error) {
+			console.error(error)
+			toast.error(`Could not import this file.`);
+		}
 	};
 </script>
 
@@ -161,7 +182,7 @@
 	<button
 		class="border-0 px-4 py-2 shadow rounded bg-yellow-500 text-white h-10 scale-90 hover:bg-yellow-600"
 		on:click={() => {
-		
+			showImportModal === true ? showImportModal = false : showImportModal = true
 		}}
 	>
 		<p class="flex">
@@ -243,4 +264,86 @@
 			<NotesRenderer {source} />
 		</div>
 	{/key}
+</div>
+
+<div hidden={showImportModal}>
+	<!-- Component: Modal with form -->
+	<!-- Modal backdrop -->
+	<div
+		class="fixed top-0 left-0 z-20 flex items-center justify-center w-screen h-screen bg-slate-300/20 backdrop-blur-sm"
+		aria-labelledby="header-4a content-4a"
+		aria-modal="true"
+		tabindex="-1"
+		role="dialog"
+	>
+		<!-- Modal -->
+		<div
+			class="flex max-h-[90vh] max-w-sm flex-col gap-4 overflow-hidden rounded bg-white p-6 text-slate-500 shadow-xl shadow-slate-700/10"
+			id="modal"
+			role="document"
+		>
+			<!-- Modal header -->
+			<header id="header-4a" class="flex items-center">
+				<h3 class="flex-1 text-lg font-medium text-slate-700">Import Note</h3>
+				<button
+					class="inline-flex items-center justify-center h-10 gap-2 px-5 text-sm font-medium tracking-wide transition duration-300 rounded-full focus-visible:outline-none justify-self-center whitespace-nowrap text-indigo-500 hover:bg-indigo-100 hover:text-indigo-600 focus:bg-indigo-200 focus:text-indigo-700 disabled:cursor-not-allowed disabled:text-indigo-300 disabled:shadow-none disabled:hover:bg-transparent"
+					aria-label="close dialog"
+					on:click={() => {
+						showImportModal === false ? showImportModal = true : showImportModal = false
+					}}
+				>
+					<span class="relative only:-mx-5">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							class="w-5 h-5"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke="currentColor"
+							stroke-width="1.5"
+							role="graphics-symbol"
+							aria-labelledby="title-79 desc-79"
+						>
+							<title id="title-79">Icon title</title>
+							<desc id="desc-79">A more detailed description of the icon</desc>
+							<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+						</svg>
+					</span>
+				</button>
+			</header>
+			<!-- Modal body -->
+			<form on:submit={handleImport}>
+				<div id="content-4a" class="flex-1">
+					<div class="flex flex-col gap-6">
+						<!-- Input field -->
+						<div class="relative mb-6">
+							<input
+								class="w-full h-10 px-4 pr-12 text-sm placeholder-transparent transition-all border-0 rounded outline-none peer border-slate-200 text-slate-500 autofill:bg-white invalid:border-pink-500 invalid:text-pink-500 focus:border-indigo-500 focus:outline-none invalid:focus:border-pink-500 disabled:cursor-not-allowed disabled:bg-slate-50 disabled:text-slate-400"
+								id="importFile"
+								type="file"
+								name="importFile"
+								accept=".md"
+								bind:files={importNote}
+							/>
+							<!-- <label for="importFile" class="absolute left-2 -top-2 z-[1] px-2 text-xs text-slate-400 transition-all before:absolute before:top-0 before:left-0 before:z-[-1] before:block before:h-full before:w-full before:bg-white before:transition-all peer-placeholder-shown:top-2.5 peer-placeholder-shown:text-sm peer-required:after:text-pink-500 peer-required:after:content-[''] peer-invalid:text-pink-500 peer-focus:-top-2 peer-focus:text-xs peer-focus:text-indigo-500 peer-invalid:peer-focus:text-pink-500 peer-disabled:cursor-not-allowed peer-disabled:text-slate-400 peer-disabled:before:bg-transparent"> Import</label> -->
+							<small
+								class="absolute flex justify-between w-full px-4 py-1 text-xs transition text-slate-400 peer-invalid:text-pink-500"
+							>
+								<span>Must be a Markdown file (.md)</span>
+							</small>
+						</div>
+					</div>
+				</div>
+				<!-- Modal actions -->
+				<div class="flex justify-center gap-2">
+					<button
+						class="inline-flex items-center justify-center w-full h-10 gap-2 px-5 text-sm font-medium tracking-wide text-white transition duration-300 rounded whitespace-nowrap bg-indigo-500 hover:bg-indigo-600 focus:bg-indigo-700 focus-visible:outline-none disabled:cursor-not-allowed disabled:border-indigo-300 disabled:bg-indigo-300 disabled:shadow-none"
+						type="submit"
+					>
+						<span>Import</span>
+					</button>
+				</div>
+			</form>
+		</div>
+	</div>
+	<!-- End Modal with form -->
 </div>
